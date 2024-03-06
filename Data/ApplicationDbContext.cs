c921d807f422c4cdd917fa2ee953c9f3042858bb
fkrsyf29 <69891145+fkrsyf29@users.noreply.github.com>
2024-03-06 17:10:58 +0700

    Add Files

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+using NetCore8CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace NetCore8CRUD.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
